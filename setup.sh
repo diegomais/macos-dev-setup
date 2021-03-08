1cb8227e7c46f20dff704b3225cf912933c3e35c
Diego Mais <diegomais.dev@gmail.com>
2021-03-08 20:55:06 -0300

    ✨ install Spotify

--- setup.sh
+++ setup.sh
@@ -106,3 +106,6 @@ brew install --cask android-studio
 
 echo "Installing: Desktop client for Amazon Music"
 brew install --cask amazon-music
+
+echo "Installing: Spotify - Music streaming service"
+brew install --cask spotify
