7e78ce2ec93f3bab247df342c4839c2da705cb59
Diego Mais <diegomais.dev@gmail.com>
2021-03-08 21:05:38 -0300

    ✨ install oh-my-zsh

--- setup.sh
+++ setup.sh
@@ -32,6 +32,9 @@ echo "Host *
   UseKeychain yes
   IdentityFile ~/.ssh/id_rsa" > ~/.ssh/config
 
+echo "Installing: oh-my-zsh"
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
 echo "Installing: Homebrew"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
